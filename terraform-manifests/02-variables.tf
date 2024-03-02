f199cb5c80c7fc032f3373b93475eaec34f5a3c8
rishabhbeni <40311833+rishabhbeni@users.noreply.github.com>
2024-03-03 05:01:18 +0530

    Update 02-variables.tf

--- terraform-manifests/02-variables.tf
+++ terraform-manifests/02-variables.tf
@@ -7,7 +7,7 @@
 variable "location" {
   type = string
   description = "Azure Region where all these resources will be provisioned"
-  default = "Central India"
+  default = "Central US"
 }
 
 # Azure Resource Group Name
