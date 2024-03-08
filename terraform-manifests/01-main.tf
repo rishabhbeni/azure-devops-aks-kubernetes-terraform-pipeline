4b974af2f22f9896d72ced6689779fc6813ef706
rishabhbeni <40311833+rishabhbeni@users.noreply.github.com>
2024-03-08 19:42:17 +0530

    Update 01-main.tf

--- terraform-manifests/01-main.tf
+++ terraform-manifests/01-main.tf
@@ -14,7 +14,8 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 2.0"
+      version = "3.94.0"
+      #version = "~> 2.0"
     }
     azuread = {
       source  = "hashicorp/azuread"
