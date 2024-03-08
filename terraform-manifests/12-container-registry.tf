b08ede3d20f4432798174b60022f339989d67ee3
rishabhbeni <40311833+rishabhbeni@users.noreply.github.com>
2024-03-08 23:32:46 +0530

    Create 12-container-registry.tf

---
+++ terraform-manifests/12-container-registry.tf
@@ -0,0 +1,12 @@
+resource "azurerm_resource_group" "aks_acr" {
+  name     = azurerm_resource_group.aks_rg.name
+  location = azurerm_resource_group.aks_rg.location
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "${azurerm_resource_group.aks_rg.name}-registry"
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  location 	      = azurerm_resource_group.aks_rg.location
+  sku                 = "standard"
+  admin_enabled       = true
+}
