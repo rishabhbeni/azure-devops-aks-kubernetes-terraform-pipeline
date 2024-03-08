0606041b029d575ebb27ac905adc6c4affab950f
rishabhbeni <40311833+rishabhbeni@users.noreply.github.com>
2024-03-08 23:53:39 +0530

    Update 12-container-registry.tf

--- terraform-manifests/12-container-registry.tf
+++ terraform-manifests/12-container-registry.tf
@@ -4,7 +4,7 @@ resource "azurerm_resource_group" "aks_acr" {
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "${azurerm_resource_group.aks_rg.name}-registry"
+  name                = "${azurerm_resource_group.aks_rg.name}registry"
   resource_group_name = azurerm_resource_group.aks_rg.name
   location 	          = azurerm_resource_group.aks_rg.location
   sku                 = "Standard"
