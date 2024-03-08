88549049d0883123c851d8184267c438cc26033c
rishabhbeni <40311833+rishabhbeni@users.noreply.github.com>
2024-03-08 23:24:23 +0530

    Create 12-container-registry.tf

---
+++ 12-container-registry.tf
@@ -0,0 +1,22 @@
+resource "azurerm_resource_group" "" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "North Europe"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
